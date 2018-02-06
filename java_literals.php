eb4b58b455caf9c704f1846e2f8a8680ebbba954
syedkhizarulhaq-cis <syedkhizarulhaq@yahoo.com>
2018-02-06 23:37:05 +0500

    java literals

---
+++ java_literals.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Strings</h2>
+
+<p>Strings can be written with double or single quotes.</p>
+
+<p id="demo"></p>
+<p id="demo1"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 'John Doe';
+
+document.getElementById("demo1").innerHTML = 10.50;
+</script>
+
+</body>
+</html>
